1a858e47f2286dacecf05d667dc2b226c5978072
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-16 21:57:39 +0200

    fin contact, manque plus qu'à changer projets

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,77 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <Navigation />
+      <div className="contactContent">
+        <div className="header"></div>
+        <div className="contactBox">
+          <h1>Contactez-moi : </h1>
+          <ul>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              <span>26 allée baptistin richelme, 83500 La Seyne Sur Mer</span>
+            </li>
+            <li>
+              <i className="fas fa-mobile-alt"></i>
+              <CopyToClipboard text="0616369919">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("Numéro de téléphone copié ! ");
+                  }}
+                >
+                  06 16 36 99 19
+                </span>
+              </CopyToClipboard>
+            </li>
+            <li>
+              <i className="far fa-envelope"></i>
+              <CopyToClipboard text="lukedussart@hotmail.fr">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("Email copié ! ");
+                  }}
+                >
+                  lukedussart@hotmail.fr
+                </span>
+              </CopyToClipboard>
+            </li>
+          </ul>
+        </div>
+        <div className="socialNetwork">
+          <ul>
+            <a
+              href="https://www.facebook.com/luke.dussart"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Facebook</h4>
+              <i className="fab fa-facebook" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/luke-dussart/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Linkedin</h4>
+              <i className="fab fa-linkedin" />
+            </a>
+            <a
+              href="https://github.com/DUSSARTLuke"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Github</h4>
+              <i className="fab fa-github" />
+            </a>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
