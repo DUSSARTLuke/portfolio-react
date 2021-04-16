0a6cd09a4fe14857e150c52fb37acf3424938e7f
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-16 18:16:22 +0200

    page home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,21 @@ import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Navigation />
+      <div className="homeContent">
+        <div className="content">
+          <h1>DUSSART Luke</h1>
+          <h2>Développeur Full-Stack</h2>
+          <div className="pdf">
+            <a href="./media/CV_DUSSART_polytech.pdf" target="_blank">
+              
+              Voir mon CV
+            
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
